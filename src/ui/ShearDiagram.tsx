3a607fc0d81cf972641cb2170fbcd608fa6a5502
Zach <53870574+Jadefire16@users.noreply.github.com>
2026-01-19 16:12:44 -0500

    Added ShearDiagram

---
+++ src/ui/ShearDiagram.tsx
@@ -0,0 +1,10 @@
+import { Sample} from "../domain/Types";
+import { Diagram} from "./Diagram";
+
+interface ShearDiagramProps {
+    data: Sample[];
+}
+
+export function ShearDiagram({ data }: ShearDiagramProps) {
+    return <Diagram data={data} title="Shear Diagram" />;
+}
