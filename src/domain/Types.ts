d1b7deedc5230843b6feaae444820d6a9e614d26
Zach <53870574+Jadefire16@users.noreply.github.com>
2026-01-19 13:58:51 -0500

    Added Domain Types
    Added the domain types first to ensure a unified structure later.

---
+++ src/domain/Types.ts
@@ -0,0 +1,38 @@
+// Defining the domain here should simplify actually rendering it.
+// Should keep the solver and UI on the same path
+
+
+// General
+export interface Sample {
+    x: number; // Position along the beam
+    value: number; // Value at position
+}
+
+// Support Types
+export enum SupportType{
+    Pinned = "Pinned",
+    Fixed = "Fixed",
+}
+
+export interface Support {
+    type: SupportType;
+    position: number; // x-position along beam
+}
+
+// Load Types
+export enum LoadType {
+    Point = "Point",
+}
+
+export interface Load {
+    type: LoadType;
+    magnitude: number;
+    position: number; // x-position along beam
+}
+
+// Beam
+export interface Beam {
+    length: number;
+    support: Support[];
+    loads: Load[];
+}
